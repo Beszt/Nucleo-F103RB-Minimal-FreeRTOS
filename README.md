55f8b9ce1a93241b4b1fd1784e1d92b0f7aec05c
Beszt <Maciej.Obarzanek@gmail.com>
2019-01-16 20:00:59 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Nucleo-F103RB-Minimal-FreeRTOS
+# Quick start
+
+1. Install drivers for [Nucleo F103RB](https://www.st.com/content/st_com/en/products/development-tools/software-development-tools/stm32-software-development-tools/stm32-utilities/stsw-link009.html) and [System Workbench for STM32](http://www.openstm32.org/Downloading%2Bthe%2BSystem%2BWorkbench%2Bfor%2BSTM32%2Binstaller) (Eclipse based IDE).
+2. Download and install HAL library for Nucleo.
+2. Download / clone repository and import it in System Workbench (Eclipse based IDE).
+3. Build and flash it!
+
+# Additional info
+
+* It works perfectly with System Workbench 2.7.0 (C/C++ Embedded Development Tools for MCU), FreeRTOS 9.0.0 and HAL Drivers V1.1.3
+* This demo version have a running task for blinking user led (PA5) on Nucleo.
---
+++ src/main.c
@@ -0,0 +1,54 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f1xx.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+void prvSetupHardware() {
+	SystemCoreClock = 8000000; // 8 mhz clock
+
+	HAL_Init();
+
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio;
+
+	gpio.Pin = GPIO_PIN_5; //Led Pin Config
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull = GPIO_NOPULL;
+	gpio.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA, &gpio);
+}
+
+void vTaskLED(void * pvParameters) {
+	for (;;) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); //Led on
+		vTaskDelay(500 / portTICK_RATE_MS);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //Led off
+		vTaskDelay(500 / portTICK_RATE_MS);
+	}
+}
+
+int main(void) {
+	//Hardware init
+	prvSetupHardware();
+
+	//Blinking led Task create
+	xTaskCreate(vTaskLED, ( const portCHAR * ) "LED", 128, NULL, 1, NULL);
+
+	//OS start (neverending scheduler function)
+	vTaskStartScheduler();
+
+	//It will never happen if everything is ok
+	return 0;
+}
